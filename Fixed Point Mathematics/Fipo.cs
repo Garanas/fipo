e10e227fcacadf40651e016407f95e24dc992d83
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-01-29 21:59:21 +0100

    Initial commit with draft version of the library

---
+++ Fixed Point Mathematics/Fipo.cs
@@ -0,0 +1,265 @@
+﻿//******************************************************************************************************
+//**Copyright(c) 2024  Willem 'Jip' Wijnia
+//**
+//** Permission is hereby granted, free of charge, to any person obtaining a copy
+//** of this software and associated documentation files (the "Software"), to deal
+//** in the Software without restriction, including without limitation the rights
+//** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//** copies of the Software, and to permit persons to whom the Software is
+//** furnished to do so, subject to the following conditions:
+//**
+//**The above copyright notice and this permission notice shall be included in all
+//** copies or substantial portions of the Software.
+//**
+//** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//** SOFTWARE.
+//******************************************************************************************************
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace FixedPointMath
+{
+    // https://github.com/MikeLankamp/fpm
+    // https://vanhunteradams.com/FixedPoint/FixedPoint.html
+
+    [DebuggerTypeProxy(typeof(FipoDebugView))]
+    [StructLayout(LayoutKind.Explicit, Pack =  4, Size = 4)]
+    public struct Fipo
+    {
+        public static readonly int Offset = 8;
+
+        /// <summary>
+        /// Quick-access multiplier for converting from a float to a fixed point number.
+        /// </summary>
+        public static readonly float FromFloatFactor = (1 << Fipo.Offset);
+
+        /// <summary>
+        /// Quick-access multiplier for converting from a fixed point number to a float.
+        /// </summary>
+        public static readonly float ToFloatFactor = 1.0f / Fipo.FromFloatFactor;
+
+        /// <summary>
+        /// Quick-access multiplier for converting from a double to a fixed point number.
+        /// </summary>
+        public static readonly double FromDoubleFactor = (1 << Fipo.Offset);
+
+        /// <summary>
+        /// Quick-access multiplier for converting from a fixed point number to a double.
+        /// </summary>
+        public static readonly double ToDoubleFactor = 1.0f / Fipo.FromDoubleFactor;
+
+        /// <summary>
+        /// Represents the fraction.
+        /// </summary>
+        [FieldOffset(0)]
+        internal byte Lower;
+
+        /// <summary>
+        /// Represents the integers.
+        /// </summary>
+        [FieldOffset(0)]
+        internal int Value;
+
+        /// <summary>
+        /// Create a fixed point number from an integer.
+        /// </summary>
+        /// <param name="value"></param>
+        public Fipo (int value)
+        {
+            this.Value = (value) << (Fipo.Offset);
+        }
+
+        /// <summary>
+        /// Create a fixed point number from a float.
+        /// </summary>
+        /// <param name="value"></param>
+        public Fipo(float value)
+        {
+            this.Value = (int)(value * FromFloatFactor);
+        }
+
+        /// <summary>
+        /// Create a fixed point number from a double.
+        /// </summary>
+        /// <param name="value"></param>
+        public Fipo(double value)
+        {
+            this.Value = (int)(value * FromDoubleFactor);
+        }
+
+        //#region Operators
+
+        /// <summary>
+        /// Add fixed point number 'b' to 'a'.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fipo operator +(Fipo a, Fipo b)
+        {
+            return new Fipo { Value = a.Value + b.Value };
+        }
+
+        /// <summary>
+        /// Subtract fixed point number 'b' from 'a'.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fipo operator -(Fipo a, Fipo b)
+        {
+            return new Fipo { Value = a.Value - b.Value };
+        }
+
+        /// <summary>
+        /// Multiply the fixed point numbers 'a' and 'b'.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fipo operator *(Fipo a, Fipo b)
+        {
+            return new Fipo { Value = a.Value - b.Value };
+        }
+
+        /// <summary>
+        /// Divide fixed point number 'a' by 'b'.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Fipo operator /(Fipo a, Fipo b)
+        {
+            return new Fipo { Value = a.Value - b.Value };
+        }
+
+        //#endregion
+
+        //#region Casts
+
+        /// <summary>
+        /// Casts a fixed point number to an integer.
+        /// </summary>
+        /// <param name="a"></param>
+        public static implicit operator int(Fipo a)
+        {
+            return a.Value >> Fipo.Offset;
+        }
+
+        /// <summary>
+        /// Casts a fixed point number to a float.
+        /// </summary>
+        /// <param name="a"></param>
+        public static implicit operator float(Fipo a)
+        {
+            return (float)a.Value * Fipo.ToFloatFactor;
+        }
+
+        /// <summary>
+        /// Casts a fixed point number to a double.
+        /// </summary>
+        /// <param name="a"></param>
+        public static implicit operator double(Fipo a)
+        {
+            return (double)a.Value * Fipo.ToDoubleFactor;
+        }
+
+        /// <summary>
+        /// Casts a fixed point number to an integer.
+        /// </summary>
+        /// <param name="a"></param>
+        public static implicit operator Fipo(int a)
+        {
+            return new Fipo { Value = a << Fipo.Offset } ;
+        }
+
+        /// <summary>
+        /// Casts a fixed point number to a float.
+        /// </summary>
+        /// <param name="a"></param>
+        public static implicit operator Fipo(float a)
+        {
+            return new Fipo { Value = (int)(a * Fipo.FromFloatFactor) };
+        }
+
+        /// <summary>
+        /// Casts a fixed point number to a double.
+        /// </summary>
+        /// <param name="a"></param>
+        public static implicit operator Fipo(double a)
+        {
+            return new Fipo { Value = (int)(a * Fipo.FromDoubleFactor) };
+        }
+
+        //#endregion
+
+        //#region Math functions
+
+        ///// <summary>
+        ///// Drop the fractions of the fixed point number.
+        ///// </summary>
+        ///// <param name="a"></param>
+        ///// <returns></returns>
+        //public static Fipo Floor(Fipo a)
+        //{
+        //    return new Fipo { Upper = a.Upper };
+        //}
+
+        ///// <summary>
+        ///// Drop the fractions of the fixed point number.
+        ///// </summary>
+        ///// <param name="a"></param>
+        ///// <returns></returns>
+        //public static Fipo Ceil(Fipo a)
+        //{
+        //    return new Fipo { Upper = (short)(a.Upper + 1) };
+        //}
+
+        ///// <summary>
+        ///// Drop the sign.
+        ///// </summary>
+        ///// <param name="a"></param>
+        ///// <returns></returns>
+        //public static Fipo Abs(Fipo a)
+        //{
+        //    return new Fipo { AllBits = Math.Abs(a.AllBits) };
+        //}
+
+        /// <summary>
+        /// Compute the square root of a fixed point number.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static Fipo SquareRoot(Fipo a)
+        {
+            double intermediate = (double)a;
+            return new Fipo(Math.Sqrt(intermediate));
+        }
+
+
+        //#endregion
+
+        public override string ToString()
+        {
+            return ((float)this).ToString("0.0000");
+        }
+
+        internal class FipoDebugView
+        {
+            public float Value = 0.0f;
+
+            // The constructor for the type proxy class must have a
+            // constructor that takes the target type as a parameter.
+            public FipoDebugView(Fipo a)
+            {
+                Value = (float)a;
+            }
+        }
+    }
+}
---
+++ Tests/IntegerProperties.cs
@@ -0,0 +1,154 @@
+using Bogus;
+
+namespace FipoTests
+{
+    public class TwoIntegerData
+    {
+        public int a; public int b;
+    }
+
+    public class TwoIntegerDataGenerator
+    {
+        private Faker<TwoIntegerData> Faker = new Faker<TwoIntegerData>();
+
+        public TwoIntegerDataGenerator(int min, int max)
+        {
+            Randomizer.Seed = new Random(6554523);
+            Faker = new Faker<TwoIntegerData>()
+                .RuleFor(u => u.a, f => f.Random.Int(min, max))
+                .RuleFor(u => u.b, f => f.Random.Int(min, max));
+        }
+
+        public TwoIntegerData GenerateIntegers()
+        {
+            return Faker.Generate();
+        }
+    }
+
+    [TestClass]
+    public class IntegerProperties
+    {
+
+        public static IEnumerable<object[]> SmallTwoIntegerData
+        {
+            get
+            {
+                var generator = new TwoIntegerDataGenerator((int)(-1 * Math.Pow(2, 8)), (int)(1 * Math.Pow(2, 8)));
+                var iterations = 100;
+                var val = new object[iterations][];
+                for (int i = 0; i < iterations; i++)
+                {
+                    var generated = generator.GenerateIntegers();
+                    val[i] = new object[] { generated.a, generated.b };
+                }
+
+                //(int)Math.Pow(2, 32 - (Fipo.Offset + 2))
+
+                return val;
+            }
+        }
+
+        public static IEnumerable<object[]> MediumTwoIntegerData
+        {
+            get
+            {
+                var iterations = 1000;
+                var generator = new TwoIntegerDataGenerator(
+                    (int)(-1 * Math.Pow(2, 16)),
+                    (int)(1 * Math.Pow(2, 16)));
+
+                var val = new object[iterations][];
+                for (int i = 0; i < iterations; i++)
+                {
+                    var generated = generator.GenerateIntegers();
+                    val[i] = new object[] { generated.a, generated.b };
+                }
+
+                //(int)Math.Pow(2, 32 - (Fipo.Offset + 2))
+
+                return val;
+            }
+        }
+
+        public static IEnumerable<object[]> LargeTwoIntegerData
+        { 
+            get
+            {
+                var generator = new TwoIntegerDataGenerator(
+                    (int)(-1 * Math.Pow(2, 22)),
+                    (int)(1 * Math.Pow(2, 22)));
+                var iterations = 10000;
+                var val = new object[iterations][];
+                for (int i = 0; i < iterations; i++)
+                {
+                    var generated = generator.GenerateIntegers();
+                    val[i] = new object[] { generated.a, generated.b };
+                }
+
+                //(int)Math.Pow(2, 32 - (Fipo.Offset + 2))
+
+                return val;
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(SmallTwoIntegerData))]
+        public void AdditionSmall(int a, int b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa + fb;
+            Assert.AreEqual((int)(fr), a + b, 0.001f);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(MediumTwoIntegerData))]
+        public void AdditionMedium(int a, int b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa + fb;
+            Assert.AreEqual((int)(fr), a + b, 0.001f);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(LargeTwoIntegerData))]
+        public void AdditionLarge(int a, int b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa + fb;
+            Assert.AreEqual((int)(fr), a + b, 0.001f);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(SmallTwoIntegerData))]
+        public void SubtractionSmall(int a, int b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa - fb;
+            Assert.AreEqual((int)(fr), a - b, 0.001f);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(MediumTwoIntegerData))]
+        public void SubtractionMedium(int a, int b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa - fb;
+            Assert.AreEqual((int)(fr), a - b, 0.001f);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(LargeTwoIntegerData))]
+        public void SubtractionLarge(int a, int b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa - fb;
+            Assert.AreEqual((int)(fr), a - b, 0.001f);
+        }
+    }
+}
