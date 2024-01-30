f631add1544dbd1411d028cc8b2687114c191b1f
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-01-30 12:15:33 +0100

    Add basic floating point tests for addition and subtraction (#1)
    Also introduces a new constant Epsilon that represents the smallest value that the library can represent.

---
+++ Tests/FloatProperties.cs
@@ -0,0 +1,154 @@
+using Bogus;
+
+namespace FipoTests
+{
+    public class TwoFloatData
+    {
+        public float a; public float b;
+    }
+
+    public class TwoFloatDataGenerator
+    {
+        private Faker<TwoFloatData> Faker = new Faker<TwoFloatData>();
+
+        public TwoFloatDataGenerator(float min, float max)
+        {
+            Randomizer.Seed = new Random(6554523);
+            Faker = new Faker<TwoFloatData>()
+                .RuleFor(u => u.a, f => Fipo.Epsilon * MathF.Floor((1 / Fipo.Epsilon) * f.Random.Float(min, max)))
+                .RuleFor(u => u.b, f => Fipo.Epsilon * MathF.Floor((1 / Fipo.Epsilon) * f.Random.Float(min, max)));
+        }
+
+        public TwoFloatData GenerateFloats()
+        {
+            return Faker.Generate();
+        }
+    }
+
+    [TestClass]
+    public class FloatProperties
+    {
+
+        public static IEnumerable<object[]> SmallTwoFloatData
+        {
+            get
+            {
+                var generator = new TwoFloatDataGenerator((float)(-1 * Math.Pow(2, 8)), (float)(1 * Math.Pow(2, 8)));
+                var iterations = 100;
+                var val = new object[iterations][];
+                for (int i = 0; i < iterations; i++)
+                {
+                    var generated = generator.GenerateFloats();
+                    val[i] = new object[] { generated.a, generated.b };
+                }
+
+                //(int)Math.Pow(2, 32 - (Fipo.Offset + 2))
+
+                return val;
+            }
+        }
+
+        public static IEnumerable<object[]> MediumTwoFloatData
+        {
+            get
+            {
+                var iterations = 1000;
+                var generator = new TwoFloatDataGenerator(
+                    (float)(-1 * Math.Pow(2, 16)),
+                    (float)(1 * Math.Pow(2, 16)));
+
+                var val = new object[iterations][];
+                for (int i = 0; i < iterations; i++)
+                {
+                    var generated = generator.GenerateFloats();
+                    val[i] = new object[] { generated.a, generated.b };
+                }
+
+                //(int)Math.Pow(2, 32 - (Fipo.Offset + 2))
+
+                return val;
+            }
+        }
+
+        public static IEnumerable<object[]> LargeTwoFloatData
+        { 
+            get
+            {
+                var generator = new TwoFloatDataGenerator(
+                    (float)(-1 * Math.Pow(2, 22)),
+                    (float)(1 * Math.Pow(2, 22)));
+                var iterations = 4000;
+                var val = new object[iterations][];
+                for (int i = 0; i < iterations; i++)
+                {
+                    var generated = generator.GenerateFloats();
+                    val[i] = new object[] { generated.a, generated.b };
+                }
+
+                //(int)Math.Pow(2, 32 - (Fipo.Offset + 2))
+
+                return val;
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(SmallTwoFloatData))]
+        public void AdditionSmall(float a, float b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa + fb;
+            Assert.AreEqual((float)(fr), a + b, Fipo.Epsilon);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(MediumTwoFloatData))]
+        public void AdditionMedium(float a, float b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa + fb;
+            Assert.AreEqual((float)(fr), a + b,  Fipo.Epsilon);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(LargeTwoFloatData))]
+        public void AdditionLarge(float a, float b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa + fb;
+            Assert.AreEqual((float)(fr), a + b, Fipo.Epsilon);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(SmallTwoFloatData))]
+        public void SubtractionSmall(float a, float b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa - fb;
+            Assert.AreEqual((float)(fr), a - b, Fipo.Epsilon);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(MediumTwoFloatData))]
+        public void SubtractionMedium(float a, float b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa - fb;
+            Assert.AreEqual((float)(fr), a - b, Fipo.Epsilon);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(LargeTwoFloatData))]
+        public void SubtractionLarge(float a, float b)
+        {
+            Fipo fa = new Fipo(a);
+            Fipo fb = new Fipo(b);
+            Fipo fr = fa - fb;
+            Assert.AreEqual((float)(fr), a - b, Fipo.Epsilon);
+        }
+    }
+}
